1c6d59f875c445e4d07c80f78189fd7d43e877b6
KittyBorgX <krishnaramasimha12@gmail.com>
2021-07-16 17:25:11 +0530

    Get tests/lex.hydra to work

--- lib/src/main.rs
+++ lib/src/main.rs
@@ -5,28 +5,11 @@ use logos::Logos;
 use std::fs::File;
 use std::io::prelude::*;
 
-fn main() {
-    let mut lex = lexer::Token::lexer("Create ridiculously fast Lexers.");
-
-    assert_eq!(lex.next(), Some(Token::Text));
-    assert_eq!(lex.span(), 0..6);
-    assert_eq!(lex.slice(), "Create");
-
-    assert_eq!(lex.next(), Some(Token::Text));
-    assert_eq!(lex.span(), 7..19);
-    assert_eq!(lex.slice(), "ridiculously");
-
-    assert_eq!(lex.next(), Some(Token::Fast));
-    assert_eq!(lex.span(), 20..24);
-    assert_eq!(lex.slice(), "fast");
-
-    assert_eq!(lex.next(), Some(Token::Text));
-    assert_eq!(lex.slice(), "Lexers");
-    assert_eq!(lex.span(), 25..31);
-
-    assert_eq!(lex.next(), Some(Token::Period));
-    assert_eq!(lex.span(), 31..32);
-    assert_eq!(lex.slice(), ".");
-
-    assert_eq!(lex.next(), None);
+fn main() -> std::io::Result<()> {
+    let mut file = File::open("./tests/lex.hydra")?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+    Ok(for token in Token::lexer(&contents) {
+        dbg!(token);
+    })
 }
