ef0bf402c4dc61df54a4bbcdabdc4a391c8a9b5a
KittyBorgX <krishnaramasimha12@gmail.com>
2021-07-16 17:35:06 +0530

    Change file names

--- lib/src/main.rs
+++ lib/src/main.rs
@@ -6,7 +6,7 @@ use std::fs::File;
 use std::io::prelude::*;
 
 fn main() -> std::io::Result<()> {
-    let mut file = File::open("./tests/lex.hydra")?;
+    let mut file = File::open("./tests/lex.hy")?;
     let mut contents = String::new();
     file.read_to_string(&mut contents)?;
     Ok(for token in Token::lexer(&contents) {
