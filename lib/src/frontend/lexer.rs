ce3e6c76208d7c5713f00e9daaf68dc1b00938e4
KittyBorgX <krishnaramasimha12@gmail.com>
2021-07-16 18:03:11 +0530

    change to .hy

---
+++ lib/src/frontend/lexer.rs
@@ -0,0 +1,24 @@
+use logos::{Lexer, Logos, Span};
+
+#[derive(Logos, Debug, PartialEq)]
+pub enum Token {
+    // Tokens can be literal strings, of any length.
+    #[token("fast")]
+    Fast,
+
+    #[token(".")]
+    Period,
+
+    // Or regular expressions.
+    #[regex("[a-zA-Z]+")]
+    Text,
+
+    // Logos requires one token variant to handle errors,
+    // it can be named anything you wish.
+    #[error]
+    // We can also use this variant to define whitespace,
+    // or any other matches we wish to skip.
+    #[regex(r"[ \t\n\f]+", logos::skip)]
+    Error,
+}
+
