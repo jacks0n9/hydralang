18d11a41ac2a4d39a776ab835208437c1d28d8f8
KittyBorgX <krishnaramasimha12@gmail.com>
2021-07-16 17:21:35 +0530

    Got the lexer to work sorta

--- lib/src/main.rs
+++ lib/src/main.rs
@@ -1,3 +1,32 @@
+#[path = "frontend/lexer.rs"]
+mod lexer;
+use lexer::Token;
+use logos::Logos;
+use std::fs::File;
+use std::io::prelude::*;
+
 fn main() {
-    println!("Hello world");
+    let mut lex = lexer::Token::lexer("Create ridiculously fast Lexers.");
+
+    assert_eq!(lex.next(), Some(Token::Text));
+    assert_eq!(lex.span(), 0..6);
+    assert_eq!(lex.slice(), "Create");
+
+    assert_eq!(lex.next(), Some(Token::Text));
+    assert_eq!(lex.span(), 7..19);
+    assert_eq!(lex.slice(), "ridiculously");
+
+    assert_eq!(lex.next(), Some(Token::Fast));
+    assert_eq!(lex.span(), 20..24);
+    assert_eq!(lex.slice(), "fast");
+
+    assert_eq!(lex.next(), Some(Token::Text));
+    assert_eq!(lex.slice(), "Lexers");
+    assert_eq!(lex.span(), 25..31);
+
+    assert_eq!(lex.next(), Some(Token::Period));
+    assert_eq!(lex.span(), 31..32);
+    assert_eq!(lex.slice(), ".");
+
+    assert_eq!(lex.next(), None);
 }
