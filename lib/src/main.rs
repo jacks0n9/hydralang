3079d957165b7e092443c36e250da40abb26d407
KittyBorgX <krishnaramasimha12@gmail.com>
2021-07-18 11:35:46 +0530

    Add LICENSE_HEADER.txt

--- lib/src/main.rs
+++ lib/src/main.rs
@@ -3,3 +3,21 @@ mod run;
 fn main() {
     run::run::run_lexer();
 }
+
+//
+// #[path = "frontend/lexer.rs"]
+// mod lexer;
+// use lexer::Token;
+// use logos::Logos;
+// use std::fs::File;
+// use std::io::prelude::*;
+//
+//
+// fn main() -> std::io::Result<()> {
+//     let mut file = File::open("./tests/lex.hy")?;
+//     let mut contents = String::new();
+//     file.read_to_string(&mut contents)?;
+//     Ok(for token in Token::lexer(&contents) {
+//         dbg!(token);
+//     })
+// }
