da7f03065e733656d3b583fe6c3c7ae90cd883f3
KittyBorgX <krishnaramasimha12@gmail.com>
2021-07-18 15:48:04 +0530

    Add file header - lexer.rs

--- lib/src/frontend/lexer.rs
+++ lib/src/frontend/lexer.rs
@@ -1,3 +1,15 @@
+//===----------------- lexer.rs - The file for lexical analysis --------------===//
+//
+// This source file is part of the hydralang open souce project
+//
+// Copyright (c) 2021 KittyBorgX and the englang project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://github.com/KittyBorgX/hydralang/blob/main/LICENSE for license information
+// See https://github.com/KittyBorgX/hydralang/blob/main/CONTRIBUTORS.md for the list of hydralang project authors
+//
+//===----------------------------------------------------------------------===//
+
 use logos::{Lexer, Logos, Span};
 
 #[derive(Logos, Debug, PartialEq)]
