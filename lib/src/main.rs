478ca27446ef1ef065a34f3f44a3ace4ea00b80c
KittyBorgX <krishnaramasimha12@gmail.com>
2021-07-18 15:45:55 +0530

    Update main.rs

--- lib/src/main.rs
+++ lib/src/main.rs
@@ -1,3 +1,16 @@
+//===----------------- main.rs - The main rust file in this project ----------===//
+//
+// This source file is part of the hydralang open souce project
+//
+// Copyright (c) 2021 KittyBorgX and the englang project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://github.com/KittyBorgX/hydralang/blob/main/LICENSE for license information
+// See https://github.com/KittyBorgX/hydralang/blob/main/CONTRIBUTORS.md for the list of hydralang project authors
+//
+//===----------------------------------------------------------------------===//
+
+#[path = "frontend/run.rs"]
 mod run;
 
 fn main() {
